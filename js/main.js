ce46a712044297c883784e862bb3ddad0d67c969
am1rcek <xusandadajonov4385@gmail.com>
2021-09-30 01:50:32 -0700

    wad

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 let elBtn = document.querySelector(".bx-menu");
 let elModal = document.querySelector(".hero__nav-bar");
 let elClose = document.querySelector(".x-icon")
+let elBody = document.querySelector("body")
 
 elBtn.addEventListener("click", function(){
     elModal.classList.add('open')
+    elBody.classList.add("hidden")
     elBtn.classList.add('remove')
     elClose.classList.remove('remove')
 })
@@ -11,6 +13,7 @@ elBtn.addEventListener("click", function(){
 elClose.addEventListener('click', function(){
     elModal.classList.remove('open')
     elBtn.classList.remove('remove')
+    elBody.classList.remove("hidden")
     elClose.classList.add('remove')
 
 })
